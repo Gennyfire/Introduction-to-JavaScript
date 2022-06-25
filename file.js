4ad18c3b0a5b850bcddcc0ac1c4a63eff37fe4c5
Gennyfire <96430313+Gennyfire@users.noreply.github.com>
2022-06-25 02:03:54 +0100

    Add files via upload

---
+++ file.js
@@ -0,0 +1,6 @@
+let myName = "My name is Akpo Generous Onowoareke, "
+let myHeight = "I'm 5ft 3inches, "
+let myCountry = "and I'm from Nigeria."
+let myInfo = myName + myHeight + myCountry
+
+alert(myInfo);
